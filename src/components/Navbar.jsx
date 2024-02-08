4e050b3c22a61cb5c542ffb8630b50e5952ba54c
ppanwar <ppanwar@cholachariots.com>
2024-02-08 20:18:49 +0530

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import Logo from './logo.png'
+
+
+
+const Navbar = () => {
+ 
+
+
+
+  return (
+    <nav className="bg-white h-[100px]
+     border-gray-200 dark:bg-gray-900 shadow-lg sticky top-0">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <div className="flex items-center ">
+        <img src={Logo} className="h-21 w-[120px] ml-[150px]" alt="" />
+
+          
+        </div>
+       
+    
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar; 
